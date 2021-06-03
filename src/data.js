3c92863af6fa1ab889197b1db32c0f9e8729f1c0
RoshRoslin <RoshRoslin@gmail.com>
2021-06-03 12:42:17 -0700

    updated data

--- src/data.js
+++ src/data.js
@@ -7,39 +7,39 @@ const sublinks = [
     links: [
       {
         index: 1,
-        label: 'Video 1',
+        label: 'Episode 1',
         icon: <FaYoutube />,
-        videoId: 'DiKMOynhNV4',
-        image: 'https://img.youtube.com/vi/DiKMOynhNV4/0.jpg',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quisquam inventore necessitatibus excepturi quia labore sequi rerum maxime atque adipisci!',
+        videoId: 'lcX6FIBYtz0',
+        image: 'https://img.youtube.com/vi/lcX6FIBYtz0/0.jpg',
+        url: 'https://youtu.be/lcX6FIBYtz0',
+        description: 'Assigning Footswitches - Effects and Hold Functions',
       },
       {
         index: 2,
-        label: 'Video 2',
+        label: 'Episode 2',
         icon: <FaYoutube />,
-        videoId: 'sSbPOIGFxus',
-        image: 'https://img.youtube.com/vi/sSbPOIGFxus/0.jpg',
-        description:
-          'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam nulla autem voluptatum repudiandae tempore commodi dolorum optio possimus illum ratione!',
+        videoId: 'vbaZjJfpr2M',
+        image: 'https://img.youtube.com/vi/vbaZjJfpr2M/0.jpg',
+        url: 'https://youtu.be/vbaZjJfpr2M',
+        description: 'Assigning Footswitches - Scenes and Toggle Functions',
       },
       {
         index: 3,
-        label: 'Video 3',
+        label: 'Episode 3',
         icon: <FaYoutube />,
-        videoId: 'oYrOk-eO5d0',
-        image: 'https://img.youtube.com/vi/oYrOk-eO5d0/0.jpg',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque similique ad nisi architecto fugiat consequuntur minus, deserunt nam corrupti iusto?',
+        videoId: 'gOeCtkBPb1g',
+        image: 'https://img.youtube.com/vi/gOeCtkBPb1g/0.jpg',
+        url: 'https://youtu.be/gOeCtkBPb1g',
+        description: 'Assigning Footswitches - Tuner and Tap Tempo',
       },
       {
         index: 4,
-        label: 'Video 4',
+        label: 'Episode 4',
         icon: <FaYoutube />,
-        videoId: 'DiKMOynhNV4',
-        image: 'https://img.youtube.com/vi/DiKMOynhNV4/0.jpg',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quisquam inventore necessitatibus excepturi quia labore sequi rerum maxime atque adipisci!',
+        videoId: 'qt4v6Euxdz0',
+        image: 'https://img.youtube.com/vi/qt4v6Euxdz0/0.jpg',
+        url: 'https://youtu.be/qt4v6Euxdz0',
+        description: 'Assigning footswitches - Preset Increment / Decrement',
       },
       {
         index: 5,
@@ -53,15 +53,6 @@ const sublinks = [
       {
         index: 6,
         label: 'Video 6',
-        icon: <FaYoutube />,
-        videoId: 'oYrOk-eO5d0',
-        image: 'https://img.youtube.com/vi/oYrOk-eO5d0/0.jpg',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque similique ad nisi architecto fugiat consequuntur minus, deserunt nam corrupti iusto?',
-      },
-      {
-        index: 7,
-        label: 'Video 7',
         icon: <FaYoutube />,
         videoId: 'oYrOk-eO5d0',
         image: 'https://img.youtube.com/vi/oYrOk-eO5d0/0.jpg',
